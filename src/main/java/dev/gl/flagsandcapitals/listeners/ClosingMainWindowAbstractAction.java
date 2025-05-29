7f67e34bf73a7c88aea3de90f7dafe60cb46199b
gl <g.lipovtsev@gmail.com>
2025-05-29 15:58:14 +0300

    Closing main window by button in PLAYING mode fixed

--- src/main/java/dev/gl/flagsandcapitals/listeners/ClosingMainWindowAbstractAction.java
+++ src/main/java/dev/gl/flagsandcapitals/listeners/ClosingMainWindowAbstractAction.java
@@ -1,12 +1,15 @@
 package dev.gl.flagsandcapitals.listeners;
 
 import dev.gl.flagsandcapitals.db.common.HyperConnection;
+import dev.gl.flagsandcapitals.enums.MainWindowMode;
 import dev.gl.flagsandcapitals.gui.MainWindow;
+import dev.gl.flagsandcapitals.utils.Configuration;
 import dev.gl.flagsandcapitals.utils.logging.Logging;
 import java.awt.event.ActionEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -24,6 +27,20 @@ public class ClosingMainWindowAbstractAction extends AbstractAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         try {
+            // blocks short cut if the player is not playng now
+            if (parent.getMainWindowMode() == MainWindowMode.PLAYING) {
+                int answer = JOptionPane.showConfirmDialog(parent,
+                        Configuration.getResourceBundle().getString("backToMainMenuDualogQuestion"),
+                        Configuration.getResourceBundle().getString("title"),
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.WARNING_MESSAGE);
+                if (answer != 0) {
+                    return;
+                }
+
+                parent.getGameModel().lose(false);
+            }
+            
             HyperConnection con = HyperConnection.getInstance();
             con.closeConnection();
 
